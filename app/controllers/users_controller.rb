5cc1fc9884059ba47118d62f704597999c29f915
amansethi00 <amansethi221306@gmail.com>
2020-05-06 18:30:04 +0530

    Created edit user details page with flash messages for successful and unsuccessful entry

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,31 @@ class UsersController < ApplicationController
     @user=User.find_by(id: params[:id])
   end
   def new
-    
+    @user=User.new
   end
   def create
     @user=User.new(name: params[:name],email: params[:email])
-    @user.save
-    redirect_to("/users/#{@user.id}")
+    if @user.save
+      flash[:notice]="You have signed up successfully!!"
+      redirect_to("/users/#{@user.id}")
+    else
+
+      render("/users/new")
+    end
+  end
+  def edit
+    @user=User.find_by(id: params[:id])
+
+  end
+  def update
+    @user=User.find_by(id: params[:id])
+    @user.name=params[:name]
+    @user.email=params[:email]
+    if @user.save
+      flash[:notice]="User details succesfully edited!!"
+      redirect_to("/users/#{@user.id}")
+    else
+      render("users/edit")
+    end
   end
 end
