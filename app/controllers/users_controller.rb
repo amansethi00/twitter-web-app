321af00d5ebb4aa8c158596f60f79cf5937dce2b
amansethi00 <amansethi221306@gmail.com>
2020-05-08 16:15:24 +0530

    New column image_name added  to database and default image added to public

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,10 @@ class UsersController < ApplicationController
     @user=User.new
   end
   def create
-    @user=User.new(name: params[:name],email: params[:email])
+    @user=User.new(name: params[:name],
+    email: params[:email],
+    image_name:"tweets_about2.png"
+    )
     if @user.save
       flash[:notice]="You have signed up successfully!!"
       redirect_to("/users/#{@user.id}")
@@ -26,6 +29,7 @@ class UsersController < ApplicationController
     @user=User.find_by(id: params[:id])
     @user.name=params[:name]
     @user.email=params[:email]
+    @user.image_name=""
     if @user.save
       flash[:notice]="User details succesfully edited!!"
       redirect_to("/users/#{@user.id}")
