e1314fa18abd2c63ab3d43f48d5f10f27dd76d4c
amansethi00 <amansethi221306@gmail.com>
2020-05-25 16:49:49 +0530

    Encrypting passwords with gem file bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
     validates:name,{presence:true}
     validates:email,{presence:true,uniqueness:true}
-    validates:password,{presence:true}
+    has_secure_password
+    #validates:password,{presence:true} not requires as method has_secure_password is used
     
     def posts
         return Post.where(user_id: self.id)
